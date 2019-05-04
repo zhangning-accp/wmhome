edd3b6b4c31c2a06c6c36964ab64e1d5760186c2
zhangning_holley <909604945@qq.com>
2019-05-04 20:55:25 +0800

    已完成第一阶段的改造，将产品详情里不需要的显示项都删除。可以部署到外网进行测试

--- public_html/includes/templates/default.catalog/views/box_product.inc.php
+++ public_html/includes/templates/default.catalog/views/box_product.inc.php
@@ -55,44 +55,44 @@
         <?php } ?>
       </div>
 
-      <div class="tax" style="margin: 0 0 1em 0;">
-       <?php if ($tax_rates) { ?>
-        <?php echo $including_tax ? language::translate('title_including_tax', 'Including Tax') : language::translate('title_excluding_tax', 'Excluding Tax'); ?>: <span class="total-tax"><?php echo currency::format($total_tax); ?></span>
-       <?php } else { ?>
-        <?php echo language::translate('title_excluding_tax', 'Excluding Tax'); ?>
-       <?php } ?>
-      </div>
+<!--      <div class="tax" style="margin: 0 0 1em 0;">-->
+<!--       --><?php //if ($tax_rates) { ?>
+<!--        --><?php //echo $including_tax ? language::translate('title_including_tax', 'Including Tax') : language::translate('title_excluding_tax', 'Excluding Tax'); ?><!--: <span class="total-tax">--><?php //echo currency::format($total_tax); ?><!--</span>-->
+<!--       --><?php //} else { ?>
+<!--        --><?php //echo language::translate('title_excluding_tax', 'Excluding Tax'); ?>
+<!--       --><?php //} ?>
+<!--      </div>-->
 
-      <?php if ($cheapest_shipping_fee !== null) { ?>
-      <div class="cheapest-shipping" style="margin: 1em 0;">
-        <?php echo functions::draw_fonticon('fa-truck'); ?> <?php echo strtr(language::translate('text_cheapest_shipping_from_price', 'Cheapest shipping from <strong class="value">%price</strong>'), array('%price' => currency::format($cheapest_shipping_fee))); ?>
-      </div>
-      <?php } ?>
+<!--      --><?php //if ($cheapest_shipping_fee !== null) { ?>
+<!--      <div class="cheapest-shipping" style="margin: 1em 0;">-->
+<!--        --><?php //echo functions::draw_fonticon('fa-truck'); ?><!-- --><?php //echo strtr(language::translate('text_cheapest_shipping_from_price', 'Cheapest shipping from <strong class="value">%price</strong>'), array('%price' => currency::format($cheapest_shipping_fee))); ?>
+<!--      </div>-->
+<!--      --><?php //} ?>
 
-      <?php if ($sku || $mpn || $gtin) { ?>
-      <div class="codes" style="margin: 1em 0;">
-        <?php if ($sku) { ?>
-        <div class="sku">
-          <?php echo language::translate('title_sku', 'SKU'); ?>:
-          <span class="value"><?php echo $sku; ?></span>
-        </div>
-        <?php } ?>
-
-        <?php if ($mpn) { ?>
-        <div class="mpn">
-          <?php echo language::translate('title_mpn', 'MPN'); ?>:
-          <span class="value"><?php echo $mpn; ?></span>
-        </div>
-        <?php } ?>
-
-        <?php if ($gtin) { ?>
-        <div class="gtin">
-          <?php echo language::translate('title_gtin', 'GTIN'); ?>:
-          <span class="value"><?php echo $gtin; ?></span>
-        </div>
-        <?php } ?>
-      </div>
-      <?php } ?>
+<!--      --><?php //if ($sku || $mpn || $gtin) { ?>
+<!--      <div class="codes" style="margin: 1em 0;">-->
+<!--        --><?php //if ($sku) { ?>
+<!--        <div class="sku">-->
+<!--          --><?php //echo language::translate('title_sku', 'SKU'); ?><!--:-->
+<!--          <span class="value">--><?php //echo $sku; ?><!--</span>-->
+<!--        </div>-->
+<!--        --><?php //} ?>
+<!---->
+<!--        --><?php //if ($mpn) { ?>
+<!--        <div class="mpn">-->
+<!--          --><?php //echo language::translate('title_mpn', 'MPN'); ?><!--:-->
+<!--          <span class="value">--><?php //echo $mpn; ?><!--</span>-->
+<!--        </div>-->
+<!--        --><?php //} ?>
+<!---->
+<!--        --><?php //if ($gtin) { ?>
+<!--        <div class="gtin">-->
+<!--          --><?php //echo language::translate('title_gtin', 'GTIN'); ?><!--:-->
+<!--          <span class="value">--><?php //echo $gtin; ?><!--</span>-->
+<!--        </div>-->
+<!--        --><?php //} ?>
+<!--      </div>-->
+<!--      --><?php //} ?>
 
       <div class="stock-status" style="margin: 1em 0;">
        <?php if ($quantity > 0) { ?>
@@ -121,49 +121,49 @@
        <?php } ?>
       </div>
 
-      <hr />
+<!--      <hr />-->
 
-      <div class="buy_now" style="margin: 1em 0;">
-        <?php echo functions::form_draw_form_begin('buy_now_form', 'post'); ?>
-        <?php echo functions::form_draw_hidden_field('product_id', $product_id); ?>
+<!--      <div class="buy_now" style="margin: 1em 0;">-->
+<!--        --><?php //echo functions::form_draw_form_begin('buy_now_form', 'post'); ?>
+<!--        --><?php //echo functions::form_draw_hidden_field('product_id', $product_id); ?>
+<!---->
+<!--        --><?php //if ($options) { ?>
+<!--          --><?php //foreach ($options as $option) { ?>
+<!--          <div class="form-group">-->
+<!--            <label>--><?php //echo $option['name']; ?><!--</label>-->
+<!--            --><?php //echo $option['description'] ? '<div>' . $option['description'] . '</div>' : ''; ?>
+<!--            --><?php //echo $option['values']; ?>
+<!--          </div>-->
+<!--          --><?php //} ?>
+<!--        --><?php //} ?>
+<!---->
+<!--        --><?php //if (!$catalog_only_mode) { ?>
+<!--        <div class="form-group">-->
+<!--          <label>--><?php //echo language::translate('title_quantity', 'Quantity'); ?><!--</label>-->
+<!--          <div style="display: flex">-->
+<!--            <div class="input-group">-->
+<!--              --><?php //echo (!empty($quantity_unit['decimals'])) ? functions::form_draw_decimal_field('quantity', isset($_POST['quantity']) ? true : 1, $quantity_unit['decimals'], 1, null) : (functions::form_draw_number_field('quantity', isset($_POST['quantity']) ? true : 1, 1)); ?>
+<!--              --><?php //echo !empty($quantity_unit['name']) ? '<div class="input-group-addon">'. $quantity_unit['name'] .'</div>' : ''; ?>
+<!--            </div>-->
+<!--            <div style="padding-left: 0.5em; white-space: nowrap;">-->
+<!--              --><?php //echo '<button class="btn btn-success" name="add_cart_product" value="true" type="submit"'. (($quantity <= 0 && !$orderable) ? ' disabled="disabled"' : '') .'>'. language::translate('title_add_to_cart', 'Add To Cart') .'</button>'; ?>
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        --><?php //} ?>
+<!---->
+<!--        --><?php //echo functions::form_draw_form_end(); ?>
+<!--      </div>-->
 
-        <?php if ($options) { ?>
-          <?php foreach ($options as $option) { ?>
-          <div class="form-group">
-            <label><?php echo $option['name']; ?></label>
-            <?php echo $option['description'] ? '<div>' . $option['description'] . '</div>' : ''; ?>
-            <?php echo $option['values']; ?>
-          </div>
-          <?php } ?>
-        <?php } ?>
+<!--      <hr />-->
 
-        <?php if (!$catalog_only_mode) { ?>
-        <div class="form-group">
-          <label><?php echo language::translate('title_quantity', 'Quantity'); ?></label>
-          <div style="display: flex">
-            <div class="input-group">
-              <?php echo (!empty($quantity_unit['decimals'])) ? functions::form_draw_decimal_field('quantity', isset($_POST['quantity']) ? true : 1, $quantity_unit['decimals'], 1, null) : (functions::form_draw_number_field('quantity', isset($_POST['quantity']) ? true : 1, 1)); ?>
-              <?php echo !empty($quantity_unit['name']) ? '<div class="input-group-addon">'. $quantity_unit['name'] .'</div>' : ''; ?>
-            </div>
-            <div style="padding-left: 0.5em; white-space: nowrap;">
-              <?php echo '<button class="btn btn-success" name="add_cart_product" value="true" type="submit"'. (($quantity <= 0 && !$orderable) ? ' disabled="disabled"' : '') .'>'. language::translate('title_add_to_cart', 'Add To Cart') .'</button>'; ?>
-            </div>
-          </div>
-        </div>
-        <?php } ?>
-
-        <?php echo functions::form_draw_form_end(); ?>
-      </div>
-
-      <hr />
-
-      <div class="social-bookmarks text-center">
-        <a class="link" href="#"><?php echo functions::draw_fonticon('fa-link', 'style="color: #333;"'); ?></a>
-        <a class="twitter" href="<?php echo document::href_link('http://twitter.com/home/', array('status' => $name .' - '. $link)); ?>" target="_blank" title="<?php echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Twitter'); ?>"><?php echo functions::draw_fonticon('fa-twitter-square fa-lg', 'style="color: #55acee;"'); ?></a>
-        <a class="facebook" href="<?php echo document::href_link('http://www.facebook.com/sharer.php', array('u' => $link)); ?>" target="_blank" title="<?php echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Facebook'); ?>"><?php echo functions::draw_fonticon('fa-facebook-square fa-lg', 'style="color: #3b5998;"'); ?></a>
-        <a class="googleplus" href="<?php echo document::href_link('https://plus.google.com/share', array('url' => $link)); ?>" target="_blank" title="<?php echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Google+'); ?>"><?php echo functions::draw_fonticon('fa-google-plus-square fa-lg', 'style="color: #dd4b39;"'); ?></a>
-        <a class="pinterest" href="<?php echo document::href_link('http://pinterest.com/pin/create/button/', array('url' => $link)); ?>" target="_blank" title="<?php echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Pinterest'); ?>"><?php echo functions::draw_fonticon('fa-pinterest-square fa-lg', 'style="color: #bd081c;"'); ?></a>
-      </div>
+<!--      <div class="social-bookmarks text-center">-->
+<!--        <a class="link" href="#">--><?php //echo functions::draw_fonticon('fa-link', 'style="color: #333;"'); ?><!--</a>-->
+<!--        <a class="twitter" href="--><?php //echo document::href_link('http://twitter.com/home/', array('status' => $name .' - '. $link)); ?><!--" target="_blank" title="--><?php //echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Twitter'); ?><!--">--><?php //echo functions::draw_fonticon('fa-twitter-square fa-lg', 'style="color: #55acee;"'); ?><!--</a>-->
+<!--        <a class="facebook" href="--><?php //echo document::href_link('http://www.facebook.com/sharer.php', array('u' => $link)); ?><!--" target="_blank" title="--><?php //echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Facebook'); ?><!--">--><?php //echo functions::draw_fonticon('fa-facebook-square fa-lg', 'style="color: #3b5998;"'); ?><!--</a>-->
+<!--        <a class="googleplus" href="--><?php //echo document::href_link('https://plus.google.com/share', array('url' => $link)); ?><!--" target="_blank" title="--><?php //echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Google+'); ?><!--">--><?php //echo functions::draw_fonticon('fa-google-plus-square fa-lg', 'style="color: #dd4b39;"'); ?><!--</a>-->
+<!--        <a class="pinterest" href="--><?php //echo document::href_link('http://pinterest.com/pin/create/button/', array('url' => $link)); ?><!--" target="_blank" title="--><?php //echo sprintf(language::translate('text_share_on_s', 'Share on %s'), 'Pinterest'); ?><!--">--><?php //echo functions::draw_fonticon('fa-pinterest-square fa-lg', 'style="color: #bd081c;"'); ?><!--</a>-->
+<!--      </div>-->
 
     </div>
   </div>
@@ -212,59 +212,59 @@
 
 </div>
 
-<script>
-  Number.prototype.toMoney = function() {
-    var number = this;
-    var decimals = <?php echo currency::$selected['decimals']; ?>;
-    var decimal_point = '<?php echo language::$selected['decimal_point']; ?>';
-    var thousands_sep = '<?php echo language::$selected['thousands_sep']; ?>';
-    var prefix = '<?php echo currency::$selected['prefix']; ?>';
-    var suffix = '<?php echo currency::$selected['suffix']; ?>';
-    var sign = (number < 0) ? '-' : '';
-
-    var i = parseInt(number = Math.abs(number).toFixed(decimals)) + '';
-    if (number - i == 0) decimals = 0;
-
-    var l = ((l = i.length) > 3) ? l % 3 : 0;
-    var f = sign + prefix + (l ? i.substr(0, l) + thousands_sep : '') + i.substr(l).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep) + (decimals ? decimal_point + Math.abs(number - i).toFixed(decimals).slice(decimals) : '') + suffix;
-
-    return f;
-  }
-
-  $('#box-product form[name=buy_now_form]').bind('input propertyChange', function(e) {
-
-    var regular_price = <?php echo currency::format_raw($regular_price); ?>;
-    var sales_price = <?php echo currency::format_raw($campaign_price ? $campaign_price : $regular_price); ?>;
-    var tax = <?php echo currency::format_raw($total_tax); ?>;
-
-    $(this).find('input[type="radio"]:checked, input[type="checkbox"]:checked').each(function(){
-      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
-      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
-      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
-    });
-
-    $(this).find('select option:checked').each(function(){
-      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
-      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
-      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
-    });
-
-    $(this).find('input[type!="radio"][type!="checkbox"]').each(function(){
-      if ($(this).val() != '') {
-      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
-      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
-      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
-      }
-    });
-
-    $('#box-product .regular-price').text(regular_price.toMoney());
-    $('#box-product .campaign-price').text(sales_price.toMoney());
-    $('#box-product .price').text(sales_price.toMoney());
-    $('#box-product .total-tax').text(tax.toMoney());
-  });
-
-  $('#box-product[data-id="<?php echo $product_id; ?>"] .social-bookmarks .link').off().click(function(e){
-    e.preventDefault();
-    prompt("<?php echo language::translate('text_link_to_this_product', 'Link to this product'); ?>", '<?php echo $link; ?>');
-  });
-</script>
+<!--<script>-->
+<!--  Number.prototype.toMoney = function() {-->
+<!--    var number = this;-->
+<!--    var decimals = --><?php //echo currency::$selected['decimals']; ?>//;
+//    var decimal_point = '<?php //echo language::$selected['decimal_point']; ?>//';
+//    var thousands_sep = '<?php //echo language::$selected['thousands_sep']; ?>//';
+//    var prefix = '<?php //echo currency::$selected['prefix']; ?>//';
+//    var suffix = '<?php //echo currency::$selected['suffix']; ?>//';
+//    var sign = (number < 0) ? '-' : '';
+//
+//    var i = parseInt(number = Math.abs(number).toFixed(decimals)) + '';
+//    if (number - i == 0) decimals = 0;
+//
+//    var l = ((l = i.length) > 3) ? l % 3 : 0;
+//    var f = sign + prefix + (l ? i.substr(0, l) + thousands_sep : '') + i.substr(l).replace(/(\d{3})(?=\d)/g, "$1" + thousands_sep) + (decimals ? decimal_point + Math.abs(number - i).toFixed(decimals).slice(decimals) : '') + suffix;
+//
+//    return f;
+//  }
+//
+//  $('#box-product form[name=buy_now_form]').bind('input propertyChange', function(e) {
+//
+//    var regular_price = <?php //echo currency::format_raw($regular_price); ?>//;
+//    var sales_price = <?php //echo currency::format_raw($campaign_price ? $campaign_price : $regular_price); ?>//;
+//    var tax = <?php //echo currency::format_raw($total_tax); ?>//;
+//
+//    $(this).find('input[type="radio"]:checked, input[type="checkbox"]:checked').each(function(){
+//      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
+//      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
+//      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
+//    });
+//
+//    $(this).find('select option:checked').each(function(){
+//      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
+//      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
+//      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
+//    });
+//
+//    $(this).find('input[type!="radio"][type!="checkbox"]').each(function(){
+//      if ($(this).val() != '') {
+//      if ($(this).data('price-adjust')) regular_price += $(this).data('price-adjust');
+//      if ($(this).data('price-adjust')) sales_price += $(this).data('price-adjust');
+//      if ($(this).data('tax-adjust')) tax += $(this).data('tax-adjust');
+//      }
+//    });
+//
+//    $('#box-product .regular-price').text(regular_price.toMoney());
+//    $('#box-product .campaign-price').text(sales_price.toMoney());
+//    $('#box-product .price').text(sales_price.toMoney());
+//    $('#box-product .total-tax').text(tax.toMoney());
+//  });
+//
+//  $('#box-product[data-id="<?php //echo $product_id; ?>//"] .social-bookmarks .link').off().click(function(e){
+//    e.preventDefault();
+//    prompt("<?php //echo language::translate('text_link_to_this_product', 'Link to this product'); ?>//", '<?php //echo $link; ?>//');
+//  });
+//</script>
